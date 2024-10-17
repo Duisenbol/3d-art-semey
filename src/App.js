02fbb0cfc09abb671dace5b5969c1740213352d0
Duisenbol <46918990+Duisenbol@users.noreply.github.com>
2024-10-17 12:09:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,53 +6,9 @@ import { TextureLoader, Vector3 } from 'three'
 
 const ART_PIECES = [
   {
-    title: 'Beautiful Beast',
-    imgPath: '/beauty_and_beast.jpeg',
-  },
-  {
-    title: 'Stillness',
-    imgPath: '/crane.jpeg',
-  },
-  {
-    title: 'Foxy',
-    imgPath: '/foxy.jpeg',
-  },
-  {
-    title: 'Gentle Giant',
-    imgPath: '/horse_sketch.jpeg',
-  },
-  {
-    title: 'Purity',
-    imgPath: '/kindness.jpeg',
-  },
-  {
-    title: 'Lonely Together',
-    imgPath: '/lonely_together.jpeg',
-  },
-  {
-    title: 'Owl',
-    imgPath: '/owl.jpeg',
-  },
-  {
-    title: 'Menace',
-    imgPath: '/panther.jpeg',
-  },
-  {
-    title: 'Paradise',
-    imgPath: '/paradise.jpeg',
-  },
-  {
-    title: 'Friendship',
-    imgPath: '/sprited_away.jpeg',
-  },
-  {
-    title: 'Wanderlust',
-    imgPath: '/wonder.jpeg',
-  },
-  {
-    title: 'Serenity',
-    imgPath: '/forest.jpeg',
-  },
+    title: 'Прикосновение света',
+    imgPath: '/touch_of_light.jpeg',
+  }
 ]
 
 const WallArt = (props) => {
